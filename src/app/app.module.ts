0f69c2862e8ed22670ef54049a208f3d2770c292
Felipe Montoya Alvarez <felipemontoyaalvarez@Woloxs-MacBook-Pro-9.local>
2019-09-04 15:51:58 -0500

    Adding first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { NavbarComponent } from './screens/dashboard/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
